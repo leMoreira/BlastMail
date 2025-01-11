7e2779e91bcbe754b9aa07b821a53fa5ad0704c4
Leandro Moreira <leandromoreira.dev@gmail.com>
2025-01-11 11:05:49 -0300

    Subindo projeto Laravel

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EmailListController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+   Auth::loginUnsigId(1);
+
+   return to_route('dashboard');
+});
+
+Route::view('/', 'welcome');
+
+Route::view('/dashboard','dashboard')->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+    Route::get('/email-list', [EmailListController::class,'index'])->name('email-list.index');
+    Route::get('/email-list/create', [EmailListController::class,'create'])->name('email-list.create');
+    Route::post('/email-list/store', [EmailListController::class,'store'])->name('email-list.store');
+
+
+});
+
+require __DIR__.'/auth.php';
